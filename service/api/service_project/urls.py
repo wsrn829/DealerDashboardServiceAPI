2b3d0b97499acb2be00f6349e515e1e6645fdbae
wsrn829 <wsrn829@gmail.com>
2024-01-19 17:10:40 -0500

    Fix sales and service urls

--- service/api/service_project/urls.py
+++ service/api/service_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("service_rest.urls")),
+    path('', include("service_rest.urls")),
 ]
