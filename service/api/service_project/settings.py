ab097fba90c964889d4df109d1c878bdc561e092
wsrn829 <wsrn829@gmail.com>
2024-01-20 08:48:35 -0500

    Update service settings.py

--- service/api/service_project/settings.py
+++ service/api/service_project/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+import os
 import dj_database_url
 from pathlib import Path
 
