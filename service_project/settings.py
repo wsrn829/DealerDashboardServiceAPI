742c7b0fa3dc452ddaf2d58cd8d1f0cab101995e
wsrn829 <wsrn829@gmail.com>
2024-01-21 20:46:52 -0500

    Update settings.py

--- service_project/settings.py
+++ service_project/settings.py
@@ -126,6 +126,7 @@ DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
